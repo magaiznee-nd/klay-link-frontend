6506c8b1956f056e8cdd8f316f9b601a73b95fa9
Sim Young Jae <hanul@hanul.me>
2022-09-10 01:55:23 +0900

    dev

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
 import { Helmet } from "react-helmet";
-import { Link } from "react-router-dom";
+import { Link, Navigate, Route, Routes } from "react-router-dom";
 
 import Layout from "../Layout";
 
 const Home = () => {
+
+    if (sessionStorage.__spa_path) {
+        const to = sessionStorage.__spa_path;
+        sessionStorage.removeItem("__spa_path");
+        return <Routes><Route element={<Navigate replace to={to}/>} /></Routes>;
+    }
+
     return (
         <Layout>
             <Helmet title="Home" />
