d92ef58859acae6fd7d85af240cee7996630d80d
dilrong <dilrong@dilrong.com>
2022-09-12 00:31:52 +0900

    feat: NotFound

--- src/views/NotFound/index.tsx
+++ src/views/NotFound/index.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Helmet } from "react-helmet";
 
 import Layout from "../Layout";
 
 const NotFound = () => {
+    useEffect(() => {
+        sessionStorage.__spa_path = location.pathname + location.search;
+        location.replace("/");
+    });
 
     return (
         <Layout>
