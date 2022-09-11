e1b445a65e7b1261017f40962f479e1d56774a9c
dilrong <dilrong@dilrong.com>
2022-09-12 00:23:51 +0900

    fix: caver

--- src/utils/Klaytn.ts
+++ src/utils/Klaytn.ts
@@ -1,15 +1,16 @@
 import { BigNumber } from "@ethersproject/bignumber";
 
 class Klaytn {
+    private caver: any | undefined = (window as any).caver;
 
-    private caver = new (window as any).Caver(new (window as any).Caver.providers.WebsocketProvider("wss://klaytn04.fandom.finance/ws/", {
-        reconnect: {
-            auto: true,
-            delay: 1000,
-            maxAttempts: true,
-            onTimeout: false
-        },
-    }));
+    // private caver = new (window as any).Caver(new (window as any).Caver.providers.WebsocketProvider("wss://klaytn04.fandom.finance/ws/", {
+    //     reconnect: {
+    //         auto: true,
+    //         delay: 1000,
+    //         maxAttempts: true,
+    //         onTimeout: false
+    //     },
+    // }));
 
     public createContract(address: string, abi: any) {
         return this.caver.contract.create(abi, address);
